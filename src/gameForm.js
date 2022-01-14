39f0219867c4b0334df939f548c10efc93e80256
Che <cgoranov@gmail.com>
2022-01-13 20:56:39 -0600

    build out of addComment method to form to add multiple comment sections dynamically

--- src/gameForm.js
+++ src/gameForm.js
@@ -4,16 +4,26 @@ class GameForm {
     addForm() {
         const formContainer = document.getElementById("form-container")
         const gameForm = document.createElement('form')
+       
         gameForm.innerHTML = 
                 `<input id="name-input" placeholder="name" type="text"/><br>
                 <input id="platform-input"  placeholder="platform" type="text"/><br>
-                <input id="comment-input1"  placeholder="comment" type="text"/><br>
-                <input id="comment-input2"  placeholder="comment" type="text"/><br>
+                <span id="comment-container">
+                    <input class="comment-input"  placeholder="comment" type="text"/>
+                </span>
+                <button data-action='add-comment'>+</button><br>
                 <input id="game-submit" value='add game' type='submit'/>`
+        
         formContainer.append(gameForm)
+
+        const addButton = document.querySelector('button')
+        
+        addButton.addEventListener("click", this.handleAddComment)
 
         gameForm.addEventListener("submit", this.handleSubmit)
     }
+
+    
 
     handleSubmit(e) {
         e.preventDefault()
@@ -38,6 +48,22 @@ class GameForm {
     handleDelete(e) {
         const li = e.target.parentElement 
         adapter.deleteGame(li)
+    }
+
+    handleAddComment(e){
+        e.preventDefault()
+        console.log("we hit this!")
+        const newComment = document.createElement("INPUT")
+        newComment.classList.add("comment-input")
+        newComment.setAttribute("type", "text")
+        newComment.setAttribute("placeholder", "comment")
+        
+
+        const commentSpan = document.querySelector('span#comment-container')
+
+        const br = document.createElement("br")
+        commentSpan.append(br)
+        commentSpan.append(newComment)
     }
 
 
